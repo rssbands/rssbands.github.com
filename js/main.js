06153b96e999e86722850fa7dd47359fab07bcf0
rssbands <43572876+rssbands@users.noreply.github.com>
2018-09-25 14:20:42 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,92 +1,52 @@
-const fillBooks = () => {
-  // получение ссылки на тело таблицы 
-  let books = document.getElementById('books');
-  booksBase.forEach(function(item, i, arr){
-  // создание строки
-  let row = document.createElement('tr');
-  // получение картинки
-  let bookImage = document.createElement('img');
-      bookImage.setAttribute('src', './images/'+ item['img'])
-      bookImage.classList.add('bookImage');
-  // оборачивание картинки в ссылку    
-  let bookLink = document.createElement('a');
-      bookLink.setAttribute('href', item['bh'])
-      bookLink.setAttribute('target', '_blank');
-      bookLink.appendChild(bookImage);    
-  // добавление размера книги    
-  let bookSize = document.createElement('h4');
-      bookSize.textContent = item['bs'];
-  // добавление всего в первую ячейку строки    
-  let book = document.createElement('td');
-      book.appendChild(bookLink);
-      book.appendChild(bookSize);
 
-  let bookDescription = document.createElement('td');
-  // название книги
-      let bookName = document.createElement('h3');    
-      bookName.textContent=item['bn'];
-  // описание книги    
-  let description = document.createElement('p');        
-      description.textContent=item['bd'];
-  // добавление всего во вторую ячейку строки    
-  let bookDescriptionContent = document.createElement('div');    
-  bookDescriptionContent.classList.add('description');    
-  bookDescriptionContent.appendChild(bookName);
-  bookDescriptionContent.appendChild(description);        
-  bookDescription.appendChild(bookDescriptionContent);
+const books = document.getElementById('library');
+const articles = document.getElementById('articles');
 
-  // заполнение строки и добавление в таблицу            
-  row.appendChild(book);
-  row.appendChild(bookDescription);
-  books.appendChild(row);    
+fetch("input.json")
+.then(response => response.json())
+.then(json => {
+    json.books.forEach(function({ bh, img, bs, bn, bd }) {
+        createBooksLibrary(books, bh, img, bs, bn, bd);
+    });
+}
+);
 
-  });
-};
+function createBooksLibrary(parent, bh, img, bs, bn, bd) {
+    const bookRow = document.createElement('div');
+    parent.appendChild(bookRow);
+    bookRow.classList.add('book-row');
+
+    const bookCover = document.createElement('div');
+    bookRow.appendChild(bookCover);
+    bookCover.classList.add('book-cover');
+
+    const bookLink = document.createElement('a');
+    bookLink.setAttribute('href', bh)
+    bookLink.setAttribute('target', '_blank');
+    bookCover.appendChild(bookLink); 
+
+    const bookCoverIcon = document.createElement('div');
+    bookLink.appendChild(bookCoverIcon);
+    bookCoverIcon.classList.add('book-cover-icon');
+    bookCoverIcon.style.backgroundImage = 'url(img/books/' + img + ')'; 
+
+    const bookDescription = document.createElement('div');
+    bookRow.appendChild(bookDescription);
+    bookDescription.classList.add('book-description');
+
+    const bookSize = document.createElement('p');
+    bookCover.appendChild(bookSize);
+    bookSize.textContent = bs;
+
+    const bookTextName = document.createElement('p');
+    bookDescription.appendChild(bookTextName);
+    bookTextName.textContent = bn;
+
+    const bookTextDescription = document.createElement('p');
+    bookDescription.appendChild(bookTextDescription);
+    bookTextDescription.textContent = bd;
+}
 
 
-const fillLinks = () => {
-  // получение ссылки на тело таблицы 
-  let books = document.getElementById('books');
-  booksBase.forEach(function(item, i, arr){
-  // создание строки
-  let row = document.createElement('tr');
-  // получение картинки
-  let bookImage = document.createElement('img');
-      bookImage.setAttribute('src', './images/'+ item['img'])
-      bookImage.classList.add('bookImage');
-  // оборачивание картинки в ссылку    
-  let bookLink = document.createElement('a');
-      bookLink.setAttribute('href', item['bh'])
-      bookLink.setAttribute('target', '_blank');
-      bookLink.appendChild(bookImage);    
-  // добавление размера книги    
-  let bookSize = document.createElement('h4');
-      bookSize.textContent = item['bs'];
-  // добавление всего в первую ячейку строки    
-  let book = document.createElement('td');
-      book.appendChild(bookLink);
-      book.appendChild(bookSize);
 
-  let bookDescription = document.createElement('td');
-  // название книги
-      let bookName = document.createElement('h3');    
-      bookName.textContent=item['bn'];
-  // описание книги    
-  let description = document.createElement('p');        
-      description.textContent=item['bd'];
-  // добавление всего во вторую ячейку строки    
-  let bookDescriptionContent = document.createElement('div');    
-  bookDescriptionContent.classList.add('description');    
-  bookDescriptionContent.appendChild(bookName);
-  bookDescriptionContent.appendChild(description);        
-  bookDescription.appendChild(bookDescriptionContent);
 
-  // заполнение строки и добавление в таблицу            
-  row.appendChild(book);
-  row.appendChild(bookDescription);
-  books.appendChild(row);    
-
-  });
-};
-
-fillBooks();
